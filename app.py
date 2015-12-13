1bfcb406b4a60885f85d81944eb0a66e7e4d4b9f
sachprem <sachin.premasuthan@gmail.com>
2015-12-13 01:30:51 -0500

    try real plot

--- app.py
+++ app.py
@@ -37,15 +37,15 @@ def graph():
     dfsort = df.sort('Day')
     dates = dfsort.Day.tolist()
     values = dfsort.Value.tolist()
-    fig = figure(title='Data from Quandle WIKI set')
+    fig = figure(title='Data from Quandle WIKI set',
               x_axis_label='date',
               x_axis_type='datetime')
-#    # Create a polynomial line graph
+   # Create a polynomial line graph
 #    x = list(range(10))
 #    fig = figure(title="Polynomial")
 #    fig.line(x, [i ** 2 for i in x], line_width=2)
     dt2 = []
-    for d in dates
+    for d in dates:
 	dt2.append(datetime.strptime(d,'%Y-%m-%d'))
     fig.line(dt2,values,line_width=2)
     script, div = components(fig)
